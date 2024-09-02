5f8c2a0dd847e11f15c39d18a4e1e1647448b287
k1105 <kanapon1105@yahoo.co.jp>
2024-09-03 00:08:20 +0900

    値の変更時のみ送信するように修正

--- sender.py
+++ sender.py
@@ -3,22 +3,25 @@ import websockets
 import json
 
 async def time_server(websocket, path):
+    prevData = ""
+
     while True:
         try:
             # yolo_results.jsonファイルの内容を読み取る
             with open("yolo_results.json", "r") as file:
                 data = json.load(file)
 
-            # JSONデータをそのまま送信
-            await websocket.send(json.dumps(data))
-            print(f"Sent data: {data}")
+            if data != prevData:
+                await websocket.send(json.dumps(data))
+                print(f"Sent data: {data}")
 
         except FileNotFoundError:
             print("yolo_results.json not found.")
         except json.decoder.JSONDecodeError:
             print("json decode error.")
 
-        # 100ms待機してから再度ファイルを読み取る
+        prevData = data
+        # 10ms待機してから再度ファイルを読み取る
         await asyncio.sleep(0.01)
 
 start_server = websockets.serve(time_server, "localhost", 8765)
