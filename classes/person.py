7f7ce72ca2d0eeb0a19479abbfd56866c45c3f2f
k1105 <kanapon1105@yahoo.co.jp>
2024-09-22 19:48:08 +0900

    speedの計算方法を修正

--- classes/person.py
+++ classes/person.py
@@ -2,7 +2,7 @@ import time
 from classes.bbox import Bbox
 
 class Person:
-    def __init__(self, id, speed, bbox: Bbox, fps=60, smoothing_factor=0.9):
+    def __init__(self, id, speed, bbox: Bbox, fps=60):
         """
         id: 人物を一意に識別するID
         speed: {'x': x方向の速度, 'y': y方向の速度} 形式の速度
@@ -15,24 +15,16 @@ class Person:
         self.bbox = bbox
         self.lostFrameCount = 0
         self.last_update_time = time.time()  # 最後に更新された時間を保存
-        self.unit_time = 1.0 / fps  # FPSに基づいた単位時間を設定
-        self.smoothing_factor = smoothing_factor  # 平滑化係数
-
+        self.unit_time = 1.0 
+        
     def update_bbox(self, new_bbox: Bbox):
-        """
-        バウンディングボックスを更新し、平滑化された速度を計算する
-        """
         current_time = time.time()
         time_diff = current_time - self.last_update_time  # 前回の更新からの時間差
 
         if time_diff > 0:
             scale_factor = self.unit_time / time_diff  # 時間差をフレーム時間にスケーリング
-            new_speed_x = (new_bbox.center()["x"] - self.bbox.center()["x"]) * scale_factor
-            new_speed_y = (new_bbox.center()["y"] - self.bbox.center()["y"]) * scale_factor
-
-            # 平滑化された速度の計算 (EMAを使用)
-            self.speed['x'] = self.smoothing_factor * self.speed['x'] + (1 - self.smoothing_factor) * new_speed_x
-            self.speed['y'] = self.smoothing_factor * self.speed['y'] + (1 - self.smoothing_factor) * new_speed_y
+            self.speed['x'] = (new_bbox.center()["x"] - self.bbox.center()["x"]) * scale_factor
+            self.speed['y'] = (new_bbox.center()["y"] - self.bbox.center()["y"]) * scale_factor
 
         self.bbox = new_bbox
         self.last_update_time = current_time  # 更新時間を記録
