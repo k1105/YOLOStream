003d39038336579d9d8ab9758626e755a8569b5d
k1105 <kanapon1105@yahoo.co.jp>
2024-10-05 17:10:54 +0900

    main.pyをwindows / iosで単一化

--- ios/main.py
+++ ios/main.py
@@ -1,16 +1,24 @@
 import subprocess
 import argparse
 import os
+import platform
 
-def run_scripts(address, mirrored):
+def run_scripts(address, mirrored, gpu):
 
-    venv = os.path.join(os.environ['VIRTUAL_ENV'], 'bin', 'python')
+    if platform.system() == "Windows":        
+        venv = os.path.join(os.environ['VIRTUAL_ENV'], 'Scripts', 'python.exe')
+    elif platform.system() == "Darwin":
+        venv = os.path.join(os.environ['VIRTUAL_ENV'], 'bin', 'python')
+    else:
+        print(platform.system()+": This is unsupported os.")
 
     # tracker.py を実行時の引数に基づいて実行
     tracker_cmd = [venv, "tracker.py"]
 
     if mirrored:
         tracker_cmd.append("--mirrored")
+    if gpu:
+        tracker_cmd.append("--gpu")
     tracker_process = subprocess.Popen(tracker_cmd)
 
     # sender.py を実行時の引数に基づいて実行
@@ -18,9 +26,6 @@ def run_scripts(address, mirrored):
     people_sender_process = subprocess.Popen(people_sender_cmd)
     pose_sender_cmd = [venv, "send_pose.py", "--address", address]
     pose_sender_process = subprocess.Popen(pose_sender_cmd)
-
-    # audio_player_cmd = [venv, "audio_player.py", "--address", address]
-    # audio_player_process = subprocess.Popen(audio_player_cmd)
 
     # 両方のプロセスが終了するまで待機
     tracker_process.wait()
@@ -33,9 +38,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run tracker and sender scripts")
     parser.add_argument("--address", help="Server address", default="localhost")
     parser.add_argument("--mirrored", help="Enable mirrored mode for tracker", action="store_true")
+    parser.add_argument("--gpu", help="Enable YOLO in gpu mode", action="store_true")
 
     # 引数をパース
     args = parser.parse_args()
 
     # パースした引数をそれぞれのスクリプトに渡して実行
-    run_scripts(args.address, args.mirrored)
+    run_scripts(args.address, args.mirrored, args.gpu)
