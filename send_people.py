f63c5ee2aea23dfb8ce77f8bfabb340e3405dcc4
k1105 <kanapon1105@yahoo.co.jp>
2024-10-05 16:06:50 +0900

    データの送信を２つの関数に分割

---
+++ send_people.py
@@ -0,0 +1,36 @@
+import asyncio
+import websockets
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--address", help="Server address", default="localhost")
+
+async def time_server(websocket, path):
+    prevData = ""
+    while True:
+        try:
+            # people_results.jsonファイルの内容を読み取る
+            with open("people_results.json", "r") as file:
+                data = json.load(file)
+
+            if data != prevData:
+                await websocket.send(json.dumps(data))
+                print(f"Sent data: {data}")
+
+        except FileNotFoundError:
+            print("people_results.json not found.")
+        except json.decoder.JSONDecodeError:
+            print("json decode error.")
+
+        prevData = data
+        # 10ms待機してから再度ファイルを読み取る
+        await asyncio.sleep(0.01)
+
+args = parser.parse_args()
+address = args.address
+
+start_server = websockets.serve(time_server, address, 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
--- send_pose.py
+++ send_pose.py
@@ -5,35 +5,29 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--address", help="Server address", default="localhost")
-parser.add_argument("--port", help="Server port", type=int, default=8765)  # ポートもオプションで指定できるように
 
-async def time_server(websocket, path):
+async def send_pose_data(websocket, path):
     prevData = ""
-
     while True:
         try:
-            # people_results.jsonファイルの内容を読み取る
-            with open("people_results.json", "r") as file:
+            with open("pose_results.json", "r") as file:
                 data = json.load(file)
 
             if data != prevData:
                 await websocket.send(json.dumps(data))
-                print(f"Sent data: {data}")
+                print(f"Sent pose data: {data}")
 
+            prevData = data
         except FileNotFoundError:
-            print("people_results.json not found.")
+            print("pose_results.json not found.")
         except json.decoder.JSONDecodeError:
             print("json decode error.")
-
-        prevData = data
-        # 10ms待機してから再度ファイルを読み取る
         await asyncio.sleep(0.01)
 
 args = parser.parse_args()
 address = args.address
-port = args.port
 
-start_server = websockets.serve(time_server, address, port)
+start_server = websockets.serve(send_pose_data, address, 8080)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
