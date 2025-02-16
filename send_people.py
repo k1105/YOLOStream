862640eba0870b1763c8b952db38707a51379f0e
k1105 <kanapon1105@yahoo.co.jp>
2025-02-16 21:12:52 +0900

    例外処理を追加

--- send_people.py
+++ send_people.py
@@ -5,32 +5,34 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--address", help="Server address", default="localhost")
-
-async def time_server(websocket, path):
-    prevData = ""
-    while True:
-        try:
-            # people_results.jsonファイルの内容を読み取る
-            with open("people_results.json", "r") as file:
-                data = json.load(file)
-
-            if data != prevData:
-                await websocket.send(json.dumps(data))
-                print(f"SENT DATA")
-
-        except FileNotFoundError:
-            print("people_results.json not found.")
-        except json.decoder.JSONDecodeError:
-            print("json decode error.")
-
-        prevData = data
-        # 10ms待機してから再度ファイルを読み取る
-        await asyncio.sleep(0.01)
-
 args = parser.parse_args()
 address = args.address
 
-start_server = websockets.serve(time_server, address, 8765)
+async def time_server(websocket, path):
+    prevData = None
+    while True:
+        try:
+            with open("people_results.json", "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            print("people_results.json not found.")
+            data = None
+        except json.decoder.JSONDecodeError:
+            print("json decode error.")
+            data = None
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+        # data が None でなければ、前回データと比較して送信
+        if data and data != prevData:
+            await websocket.send(json.dumps(data))
+            print("SENT DATA")
+
+        prevData = data
+        await asyncio.sleep(0.01)
+
+async def main():
+    async with websockets.serve(time_server, address, 8765):
+        print(f"WebSocket server started on {address}:8765")
+        await asyncio.Future()  # 無限に実行するための待機
+
+if __name__ == "__main__":
+    asyncio.run(main())
